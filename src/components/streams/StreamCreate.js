6752358a6efa6acc6ba2ee6870374bb7ff288566
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-03-08 00:47:59 +0530

    Handle form submit

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends Component {
-  renderInput(formProps) {
+  renderInput({ input, label }) {
     /* return (
       <input
         onChange={formProps.input.onChange}
@@ -10,14 +10,31 @@ class StreamCreate extends Component {
       />
     ) */
 
-    return <input {...formProps.input} />
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    )
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues)
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     )
   }
